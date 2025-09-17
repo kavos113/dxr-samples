00ec56404363fa59b95e1689eb033b57e9607025
kavos113 <kohei.hadano@gmail.com>
2025-09-17 09:35:59 +0900

    init application

---
+++ Application.h
@@ -0,0 +1,32 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <windows.h>
+
+#include <memory>
+#include "D3DEngine.h"
+
+class Application
+{
+public:
+    Application();
+    ~Application();
+
+    int createWindow(int x = CW_USEDEFAULT, int y = CW_USEDEFAULT, int width = 800, int height = 600);
+
+    void run();
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    LRESULT handleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    std::unique_ptr<D3DEngine> m_engine;
+    HWND m_hwnd;
+
+    const wchar_t* className = L"ApplicationWindowClass";
+};
+
+#endif //APPLICATION_H
---
+++ D3DEngine.h
@@ -0,0 +1,17 @@
+#ifndef D3DENGINE_H
+#define D3DENGINE_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+class D3DEngine
+{
+};
+
+
+#endif //D3DENGINE_H
